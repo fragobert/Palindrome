6565dc945fdba2e70046924dac2553f710fd09e1
Fragobert <staflerflorian@gmail.com>
2022-09-19 14:16:59 +0200

    asdf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
+    //method that checks if a string is a palindrome
+    public static boolean isPalindrome(String str) {
+        //remove all spaces and special characters
+        str = str.replaceAll("[^a-zA-Z0-9]", "");
+        System.out.println("str = " + str);
+
+        //converts the string to lowercase
+        str = str.toLowerCase();
+        //converts the string to a character array
+        char[] charArray = str.toCharArray();
+        //creates a string builder object
+        StringBuilder sb = new StringBuilder();
+        //appends the characters of the character array to the string builder object
+        for (char c : charArray) {
+            sb.append(c);
+        }
+        //reverses the string builder object
+        sb.reverse();
+        //converts the string builder object to a string
+        String reverse = sb.toString();
+        //checks if the string is a palindrome
+        if (str.equals(reverse)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s1 = "Won’t lovers revolt now?";
+        String s2 = "Das ist kein Palindrom";
+        System.out.println(isPalindrome(s1));
+
+        //print hello world
+
+
+
+
+
+
+
+
     }
 }
